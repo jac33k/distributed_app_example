5a0faa93e18d93c1a9c684e8b497588db186395c
Jacek Brożek <jac33k@gmail.com>
2014-09-24 20:46:40 +0200

    [ui_app] invitations controller improvements + view

--- ui_app/app/controllers/invitations_controller.rb
+++ ui_app/app/controllers/invitations_controller.rb
@@ -1,6 +1,16 @@
 class InvitationsController < ApplicationController
 
   def index
-    
+  end
+
+  def create
+    InvitationSend.new(invitation_params).call
+    flash[:notice] = "Invitation has been sent!"
+    redirect_to invitations_url
+  end
+
+  private
+  def invitation_params
+    params.permit(:name, :email)
   end
 end
