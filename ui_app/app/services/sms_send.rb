a77196ec3dee1a27e80524104d1274a92818cd29
Jacek Brożek <jac33k@gmail.com>
2014-09-24 16:24:23 +0200

    [ui_app] service for sending SMSs

---
+++ ui_app/app/services/sms_send.rb
@@ -0,0 +1,27 @@
+class SmsSend
+
+  def initialize(opts = {})
+    @options = opts.to_hash
+  end
+
+  def call
+    conn.start
+
+    channel.default_exchange.publish(Marshal.dump(@options), routing_key: queue.name)
+
+    conn.close
+  end
+
+  private
+  def conn
+    @conn ||= Bunny.new(automatically_recover: false)
+  end
+
+  def channel
+    @channel ||= conn.create_channel
+  end
+
+  def queue
+    @queue ||= channel.queue("sms_app")
+  end
+end
